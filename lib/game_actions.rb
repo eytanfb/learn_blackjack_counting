4d17675d8bc9721bc8c8836480be5e0647879bf4
Eytan Anjel <anjel@usc.edu>
2013-05-10 03:57:36 -0700

    game actions initializing

--- lib/game_actions.rb
+++ lib/game_actions.rb
@@ -1,7 +1,20 @@
 class GameActions
   
-  def initialize(decks)
+  attr_reader :dealer
+  
+  def initialize(decks, players, dealer)
     @decks = decks
+    @players = []
+    @dealer = dealer
+    @players << players
+  end
+  
+  def number_of_decks
+    @decks.count
+  end
+  
+  def number_of_players
+    @players.count
   end
   
 end
--- test/tc_game_actions.rb
+++ test/tc_game_actions.rb
@@ -1,12 +1,22 @@
 require 'test/unit'
 require './lib/game_actions'
 require './lib/card_deck'
+require './lib/player'
+
 
 class TestGameActions < Test::Unit::TestCase
   
   def setup
     decks = [CardDeck.new, CardDeck.new]
-    @game_actions = GameActions.new(decks)
+    player = Player.new "Eytan"
+    dealer = Player.new "Dealer"
+    @game_actions = GameActions.new(decks, player, dealer)
+  end
+  
+  def test_init
+    assert_equal(2, @game_actions.number_of_decks)
+    assert_equal(1, @game_actions.number_of_players)
+    assert_not_nil @game_actions.dealer
   end
   
 end
