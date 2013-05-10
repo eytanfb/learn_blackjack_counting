28d8680175cfce15ac92a25effb3e62f536b100d
Eytan Anjel <anjel@usc.edu>
2013-05-10 04:07:08 -0700

    turning the game into a single-deck

--- lib/game_actions.rb
+++ lib/game_actions.rb
@@ -2,19 +2,25 @@ class GameActions
   
   attr_reader :dealer
   
-  def initialize(decks, players, dealer)
-    @decks = decks
+  def initialize(deck, players, dealer)
+    @deck = deck
     @players = []
     @dealer = dealer
     @players << players
   end
   
   def number_of_decks
-    @decks.count
+    1
   end
   
   def number_of_players
     @players.count
+  end
+  
+  def deal
+    2.times do
+      @players.each(receive_card())
+    end
   end
   
 end
--- test/tc_game_actions.rb
+++ test/tc_game_actions.rb
@@ -7,14 +7,14 @@ require './lib/player'
 class TestGameActions < Test::Unit::TestCase
   
   def setup
-    decks = [CardDeck.new, CardDeck.new]
+    deck = CardDeck.new
     player = Player.new "Eytan"
     dealer = Player.new "Dealer"
-    @game_actions = GameActions.new(decks, player, dealer)
+    @game_actions = GameActions.new(deck, player, dealer)
   end
   
   def test_init
-    assert_equal(2, @game_actions.number_of_decks)
+    assert_equal(1, @game_actions.number_of_decks)
     assert_equal(1, @game_actions.number_of_players)
     assert_not_nil @game_actions.dealer
   end
